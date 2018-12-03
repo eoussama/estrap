181e6c9ce812bb61194199424f11ecac3336dc8e
Eoussama <oussama_gtacj@live.fr>
2018-12-03 12:36:57 +0000

    Basic content generation added.

--- src/templates/node/public/assets/js/main.js
+++ src/templates/node/public/assets/js/main.js
@@ -1,12 +1,15 @@
+/**
+*
+* @name:       myNodeProject
+* @version:    0.1.0
+* @author:     EOussama
+* @license     MIT
+* @source:     github.com/EOussama/myNodeProject
+* 
+* The main javascript file of the app.
+*/
 
-
-
-
-
-
-
-
-
-
-
-
+window.addEventListener('load', () => {
+    console.log('Hello, world!');
+});
+    
