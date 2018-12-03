4cc0d9057f4ad01699f9b19950bb9c3d39b00224
Eoussama <oussama_gtacj@live.fr>
2018-12-03 01:06:51 +0000

    Automatically creating primary templates.

--- src/estrap.sh
+++ src/estrap.sh
@@ -11,8 +11,9 @@
 # -----------------------------------------------------------
 
 
-# Shows an error message and pauses the script.
-Error() {
+
+# Shows a message and pauses the script.
+Pause() {
     echo "[Estrap] $1"
     read -p "Press any key in order to continue: "
 }
@@ -25,9 +26,19 @@ CreateFolder() {
     fi
 }
 
+# Creates a file.
+CreateFile() {
+    touch "$1/$2/$3"
+    echo "$4" >> "$1/$2/$3"
+}
+
+
 
 # The project's name.
 PROJECT_NAME=$1
+
+# The template's name.
+TEMPLATE_NAME=$2
 
 # The directory of the bash script.
 ESTRAP_PATH=`dirname $0`
@@ -36,17 +47,68 @@ ESTRAP_PATH=`dirname $0`
 CURRENT_PATH=$(pwd)
 
 
+
 # Creating the templates' folder.
 CreateFolder $ESTRAP_PATH "templates"
 
-# Check if the project's name is passed.
-if [ -z "$PROJECT_NAME" ]
-then
-    Error "Usage: estrap --project-name"
-else
-    # Creating the project's folder.
-    CreateFolder $CURRENT_PATH $PROJECT_NAME
+# Creating the html template.
+CreateFolder $ESTRAP_PATH "templates/html"
+CreateFolder $ESTRAP_PATH "templates/html/assets"
+CreateFolder $ESTRAP_PATH "templates/html/assets/js"
+CreateFolder $ESTRAP_PATH "templates/html/assets/css"
+CreateFolder $ESTRAP_PATH "templates/html/assets/img"
+CreateFile $ESTRAP_PATH "templates/html" "index.html" ""
+CreateFile $ESTRAP_PATH "templates/html/assets/js" "main.js" ""
+CreateFile $ESTRAP_PATH "templates/html/assets/css" "main.css" ""
 
-    # End message.
-    echo "[Estrap] $PROJECT_NAME was successfully created!"
+# Creating the php template.
+CreateFolder $ESTRAP_PATH "templates/php"
+CreateFolder $ESTRAP_PATH "templates/php/config"
+CreateFolder $ESTRAP_PATH "templates/php/controllers"
+CreateFolder $ESTRAP_PATH "templates/php/models"
+CreateFolder $ESTRAP_PATH "templates/php/public"
+CreateFolder $ESTRAP_PATH "templates/php/public/assets"
+CreateFolder $ESTRAP_PATH "templates/php/public/assets/js"
+CreateFolder $ESTRAP_PATH "templates/php/public/assets/css"
+CreateFolder $ESTRAP_PATH "templates/php/public/assets/img"
+CreateFolder $ESTRAP_PATH "templates/php/public/views"
+CreateFolder $ESTRAP_PATH "templates/php/public/views/partials"
+CreateFolder $ESTRAP_PATH "templates/php/utils"
+CreateFile $ESTRAP_PATH "templates/php" ".htaccess" ""
+CreateFile $ESTRAP_PATH "templates/php/config" "config.ini" ""
+CreateFile $ESTRAP_PATH "templates/php/config" "config.php" ""
+CreateFile $ESTRAP_PATH "templates/php/public" "index.php" ""
+CreateFile $ESTRAP_PATH "templates/php/public/assets/js" "main.js" ""
+CreateFile $ESTRAP_PATH "templates/php/public/assets/css" "main.css" ""
+
+# Creating the node template.
+CreateFolder $ESTRAP_PATH "templates/node"
+CreateFolder $ESTRAP_PATH "templates/node/models"
+CreateFolder $ESTRAP_PATH "templates/node/public"
+CreateFolder $ESTRAP_PATH "templates/node/public/assets"
+CreateFolder $ESTRAP_PATH "templates/node/public/assets/js"
+CreateFolder $ESTRAP_PATH "templates/node/public/assets/css"
+CreateFolder $ESTRAP_PATH "templates/node/public/assets/img"
+CreateFolder $ESTRAP_PATH "templates/node/views"
+CreateFolder $ESTRAP_PATH "templates/node/views/partials"
+CreateFile $ESTRAP_PATH "templates/node" "app.js" ""
+CreateFile $ESTRAP_PATH "templates/node/public/assets/js" "main.js" ""
+CreateFile $ESTRAP_PATH "templates/node/public/assets/css" "main.css" ""
+
+# Check if the project's name is passed.
+if [ -z "$PROJECT_NAME" ] || [ -z "$TEMPLATE_NAME" ]
+then
+    Pause "Usage: estrap --project-name --template-name"
+else
+    # Check if the template exists.
+    if [ -e "$ESTRAP_PATH/templates/$TEMPLATE_NAME" ]
+    then
+        # Creating the project's folder.
+        CreateFolder $CURRENT_PATH $PROJECT_NAME
+
+        # Displaying the end message.
+        Pause "“$PROJECT_NAME” was successfully created!"
+    else
+        Pause "The template “$TEMPLATE_NAME” doesn't exist."
+    fi
 fi
